b0a1c316b10a2c6fb6fbf433417a22bb16c47c3b
Inferno20055 <baanovaleksej676@gmail.com>
2024-09-09 13:20:45 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <random>
+#include <cmath>
+using namespace std;
+void plus1(float x1, float y1) {
+	cout << "ответ" << x1 + y1;
+}
+int main() {
+	setlocale(LC_ALL, "Ru");
+	//int a;
+	//float x, y;
+	//ar z;
+	//ut << "введите символ\n";
+	//cin >> a;
+	//cout << "введите число";
+	//cin >> x;
+	//cout << "Введите число";
+	//cin >> y;
+	//cin >> z;
+//	cout << "hello world  " << a," \n";
+
+
+
+	//switch(z)
+	//{
+	//case'+':
+		//plus1(x, y);
+		//break;
+	//}
+	float a, b;// инециализировал данные
+	cout << "введите число:";
+	cin >> a;//введения первого числа
+	cout << "введите число:";
+	cin >> b;//введение второго числа
+	cout << "ответ" << a - b << "\n";//вычитания двух чисел
+	cout << "ответ" << a * b << "\n";//умножения двух чисел
+	cout << "ответ" << a + b << "\n";//сложение двух чисел
+	cout << "ответ" << a / b << "\n";//деление двух чисел
+	int l;//
+	const float p = 3.14;//
+	cout << "Введите длину окружности:";//введите число
+	cin >> l;//
+	cout << "S=" << (l * l) / (4 * p) << "\n";//
+	int c;
+	cout << "Введите число:";
+	cin >> c;
+	cout << "Ответ:" << (c * c) * (c * c);
+	cout << "купи гораж";
+	cout << "йоу";
+
+
+
+
+
+
+
+
+
+	return 0;
+}
