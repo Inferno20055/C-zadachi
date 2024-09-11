eed14c26df67e67a6ad385c0d8e57cc8b3d6246f
Inferno20055 <baanovaleksej676@gmail.com>
2024-09-11 19:16:55 +0900

    Add files via upload

---
+++ задание 11ж/задание 11ж.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    float x, y, z;
+    int a, b;
+    cout << "Введите переменную х:";
+    cin >> x;
+    cout << "Введите переменную y:";
+    cin >> y;
+    cout << "Введите переменную z:";
+    cin >> z;
+    a=
+    b = (x - (x * x) / (1 * 2 * 3)) + ((x * x * x * x * x) / (1 * 2 * 3 * 4 * 5));
+    cout << "otvet" << b;
+}
+
