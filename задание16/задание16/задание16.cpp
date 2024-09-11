72439ae10b40f2d3a99404872db297ca45c2ff03
Inferno20055 <baanovaleksej676@gmail.com>
2024-09-11 19:09:28 +0900

    Add files via upload

---
+++ задание16/задание16/задание16.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+	cout << "zadanie 1";
+	int l;//инициализоровал переменную
+	const float p = 3.14;//число р
+	cout << "Введите длину окружности:";//сообщение для пользователя
+	cin >> l;// введения переменной
+	cout << "S=" << (l * l) / (4 * p) << "\n";//формула вычесления
+}
+
