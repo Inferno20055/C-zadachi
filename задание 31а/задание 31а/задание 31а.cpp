d50c0429d93df7a7c2bc9e7b7c5620e2f30e28be
Inferno20055 <baanovaleksej676@gmail.com>
2024-09-11 19:16:55 +0900

    Add files via upload

---
+++ задание 31а/задание 31а/задание 31а.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{ setlocale(LC_ALL, "Ru");
+cout << "zadanie 3\n";
+int a;//инициализировал переменную
+cout << "Введите число:";//подсказка для пользователя
+cin >> a;//команда для ввендения переменной
+double a2 = a * a;//1 часть формулы а^4
+double a4 = a2 * a2;//2 часть формулы a^4
+cout << "а4= " << a4 << endl;//ответ и окончание формулы
+
+}
