6dbd16179c6316c1d419de6bec6574dc028601cd
Inferno20055 <baanovaleksej676@gmail.com>
2024-09-11 19:16:55 +0900

    Add files via upload

--- задание 11ж/задание 11ж.cpp
+++ задание 11ж/задание 11ж.cpp
@@ -1,18 +1,22 @@
 ﻿#include <iostream>
+#include <math.h>
+#include<cmath>
 using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Ru");
     float x, y, z;
-    int a, b;
+    float a, b;
+    const float e = 2.71;
     cout << "Введите переменную х:";
     cin >> x;
     cout << "Введите переменную y:";
     cin >> y;
     cout << "Введите переменную z:";
     cin >> z;
-    a=
+    a =  (e /abs((y-sqrt(abs(x)))*x-(y/z+(x*x/4))));
     b = (x - (x * x) / (1 * 2 * 3)) + ((x * x * x * x * x) / (1 * 2 * 3 * 4 * 5));
-    cout << "otvet" << b;
+    cout << "Ответ части а:"<< a<<"\n";
+    cout << "Ответ части b:" << b;
 }
 
