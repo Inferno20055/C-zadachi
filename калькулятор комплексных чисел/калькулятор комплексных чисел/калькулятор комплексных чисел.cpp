e5dc718f4200fcb9f0bbb9a098a83751d2f32eed
Inferno20055 <baanovaleksej676@gmail.com>
2024-09-22 20:21:49 +0900

    Add files via upload

---
+++ калькулятор комплексных чисел/калькулятор комплексных чисел/калькулятор комплексных чисел.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int a1, b1, a2, b2, z,z1;
+    cout << "Введите значение а1,b1,a2,b2:";
+    cin >> a1>>b1>>a2>>b2;
+    cout << "z1=" << a1 << "+" << b1<<"i\n";
+    cout << "z2=" << a2 << "+" << b2 << "i\n";
+    cout <<"Формула сложения:"<< "z1" << "+" << "z2\n";
+    z = (a1 + a2);
+    z1 = (b1 + b2);
+    cout <<"Ответ сложения комплексных чисел:" << z << "+" << z1<<"i";
+
+}
